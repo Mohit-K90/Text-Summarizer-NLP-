a5c756f3ab7d613d72d785f89210995ca8790181
Mohit-K90 <mohitkamkhalia@gmail.com>
2025-01-11 22:25:23 +0530

    added data validation

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -7,3 +7,9 @@ class DataIngestionConfig:
         self.source_URL = source_URL
         self.local_data_file = local_data_file
         self.unzip_dir = unzip_dir
+
+class DataValidationConfig:
+    def __init__(self, root_dir: Path, STATUS_FILE: str, ALL_REQUIRED_FILES: list):
+        self.root_dir = root_dir
+        self.STATUS_FILE = STATUS_FILE
+        self.ALL_REQUIRED_FILES = ALL_REQUIRED_FILES
