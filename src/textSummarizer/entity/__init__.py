7e3d7b393b4a35202dd3a50504135b0dd41cc12f
Mohit-K90 <mohitkamkhalia@gmail.com>
2025-01-11 19:32:15 +0530

    Added Data Ingestion

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,9 @@
+# In textSummarizer/entity/__init__.py
+from pathlib import Path
+
+class DataIngestionConfig:
+    def __init__(self, root_dir: Path, source_URL: str, local_data_file: Path, unzip_dir: Path):
+        self.root_dir = root_dir
+        self.source_URL = source_URL
+        self.local_data_file = local_data_file
+        self.unzip_dir = unzip_dir
